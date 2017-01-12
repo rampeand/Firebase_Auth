3713db61438490cc605988665af1005411e0669e
rampeand <andre.raden+github@gmail.com>
2017-01-12 02:18:37 -0500

    Login Base

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(function () {
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyACvce20Ozr28TInecNWN7yC3PXJkdmN9k",
+    authDomain: "authtest-762b1.firebaseapp.com",
+    databaseURL: "https://authtest-762b1.firebaseio.com",
+    storageBucket: "authtest-762b1.appspot.com",
+    messagingSenderId: "499424038377"
+  };
+  firebase.initializeApp(config);
+
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+
+  //login event
+
+  btnLogin.addEventListener('click', e => {
+    //get email and pwd
+    const email = txtEmail.value;
+    const password = txtPassword.value;
+    const auth = firebase.auth();
+
+    //sign in
+
+    const promise = auth.signInWithEmailAndPassword(email, password);
+
+    promise.catch(e=> console.log(e.message));
+  });
+
+}());
